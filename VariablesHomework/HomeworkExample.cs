816708d338ad7bef36bf3c8a9a9a9ccf71522876
Aiden <berlingeri_aiden@hotmail.com>
2022-04-13 15:02:37 -0400

    Added finishing quote and corrected statement to be true.

--- VariablesHomework/HomeworkExample.cs
+++ VariablesHomework/HomeworkExample.cs
@@ -12,7 +12,7 @@ namespace VariablesExample
             // This line contains a syntax error.
             // It prevents the solution from building,
             // so it will not pass.
-            Console.WriteLine("Hello World);
+            Console.WriteLine("Hello World");
 
 
 
@@ -21,7 +21,7 @@ namespace VariablesExample
             // logical error - it "asserts" that a condition
             // is true, but the condition is false,
             // so the test will not pass.
-            Assert.IsTrue(1 == 2);
+            Assert.IsTrue(2 == 2);
         }
 
     }
